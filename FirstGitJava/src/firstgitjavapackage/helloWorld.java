d06d4811dbd92cbf358fc84ebc248370481e8813
Yaksh Ujoodha <yakshujoodha@gmail.com>
2024-09-12 20:29:30 -0500

    Adding HelloWorld.java

---
+++ FirstGitJava/src/firstgitjavapackage/helloWorld.java
@@ -0,0 +1,10 @@
+package firstgitjavapackage;
+
+public class helloWorld {
+	    public static void main(String[] args) {
+
+	        // Prints "Hello, World" in the terminal window.
+	        System.out.println("Hello, World");
+	    }
+
+}
