2df870f2368c457959456229b23aa06f55e069af
liceyoung <aliceduong841@gmail.com>
2024-09-12 22:09:09 -0500

    changes made by Alice

--- FirstGitJava/src/firstgitjavapackage/helloWorld.java
+++ FirstGitJava/src/firstgitjavapackage/helloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 	        // Prints "Hello, World" in the terminal window.
 	        System.out.println("Hello, World");
-          System.out.println("My name is Yaksh");
+          System.out.println("My name is Yaksh, and my name is Alice");
           System.out.println("I have been in Beloit for 3 years and 9 months");
 	    }
 }
